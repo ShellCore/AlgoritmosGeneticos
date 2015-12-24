767e71c7f6863f878e2a3af6aa3da3bf07265bb5
Cesar Morales Garduño <shell.one.core@gmail.com>
2015-12-23 19:02:41 -0600

    Se implementaron algunas funciones del algoritmo de evaluación del algoritmo genético del zudoku.

--- src/AlgoritmoGenerticoZudoku.java
+++ src/AlgoritmoGenerticoZudoku.java
@@ -45,19 +45,83 @@ public class AlgoritmoGenerticoZudoku extends GeneticAlgorithm<Integer> {
     }
 
     private long evaluateRow(Integer[] casoAProbar, Chromosome<Integer> chromosome) {
-        return 0;
+        long res = 0;
+        for (int row = 0; row < 9; row++) {
+            List<Integer> evaluados = new ArrayList<>();
+            for (int column = 0; column < 9; column++) {
+                if ((casoAProbar[(row * 9) + column]) != 0) {
+                    evaluados.add(casoAProbar[(row * 9) + column]);
+                }
+            }
+            for (int column = 0; column < 9; column++) {
+                if (evaluados.contains(chromosome.getGens()[(row * 9) + column])) {
+                    res++;
+                } else {
+                    evaluados.add(chromosome.getGens()[(row * 9) + column]);
+                }
+            }
+        }
+
+        return res;
     }
 
     private long evaluateColumn(Integer[] casoAProbar, Chromosome<Integer> chromosome) {
-        return 0;
+        long res = 0;
+        for (int col = 0; col < 9; col++) {
+            List<Integer> evaluados = new ArrayList<>();
+            for (int row = 0; row < 9; row++) {
+                if ((casoAProbar[col + (row * 9)]) != 0) {
+                    evaluados.add(casoAProbar[col+(row * 9)]);
+                }
+            }
+            for (int row = 0; row < 9; row++) {
+                if (evaluados.contains(chromosome.getGens()[col + (row * 9)])) {
+                    res++;
+                } else {
+                    evaluados.add(chromosome.getGens()[col + (row * 9)]);
+                }
+            }
+        }
+
+        return res;
     }
 
     private long evaluateBlock(Integer[] casoAProbar, Chromosome<Integer> chromosome) {
-        return 0;
+        long res = 0;
+        for (int bloque = 0; bloque < 9; bloque++) {
+            List<Integer> evaluados = new ArrayList<>();
+            for (int col = 0; col < 3; col++) {
+                for (int fila = 0; fila < 3; fila++) {
+                    int celda = casoAProbar[(bloque*3) + (col * 9) + fila];
+                    if ((celda) != 0) {
+                        evaluados.add(celda);
+                    }
+                }
+            }
+            for (int col = 0; col < 3; col++) {
+                for (int fila = 0; fila < 3; fila++) {
+                    int celda = chromosome.getGens()[(bloque*3) + (col * 9) + fila];
+                    if (evaluados.contains(celda)) {
+                        res++;
+                    } else {
+                        evaluados.add(celda);
+                    }
+                }
+            }
+        }
+
+        return res;
     }
 
     private long evaluateDiagonal(Integer[] casoAProbar, Chromosome<Integer> chromosome) {
-        return 0;
+        long res = 0;
+
+        List<Integer> evaluados = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            // TODO
+        }
+
+        return res;
     }
 
 
